e504f7dca9d915c33d3601270476e7fb0d404e91
Corentin <co@tmb.io>
2013-04-03 13:55:34 +0200

    removed client secret from config.php

--- app/config.php
+++ app/config.php
@@ -1,4 +1,4 @@
 <?php
 // Create an app at https://code.google.com/apis/console/
-$google_oauth_client_id = '933335039618.apps.googleusercontent.com';
-$google_oauth_client_secret = 'HWsPujtQXZF_Rqm_AJhAIcix';
+$google_oauth_client_id = 'paste client id';
+$google_oauth_client_secret = 'page client secret';
